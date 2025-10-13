9157cbed7bea73fc81918f42df22b215552f9822
장정우 <jungwoo@lotte.net>
2025-10-13 15:54:39 +0900

    20251013 - 실전에서 바로 써먹는 Kafka 입문 - 컨슈머

---
+++ kafka-email-send-consumer/src/main/java/com/example/emailsendconsumer/KafkaEmailSendConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailsendconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEmailSendConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaEmailSendConsumerApplication.class, args);
+    }
+
+}
---
+++ kafka-email-send-consumer/src/test/java/com/example/emailsendconsumer/KafkaEmailSendConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailsendconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaEmailSendConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
