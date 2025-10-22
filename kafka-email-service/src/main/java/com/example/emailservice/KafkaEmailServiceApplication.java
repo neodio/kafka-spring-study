e51aae571947d2cfa0c5169bfce6532a5d563355
장정우 <jungwoo@lotte.net>
2025-10-22 18:28:11 +0900

    20251022 - 실전에서 바로 써먹는 Kafka 입문 - email-service 구현

---
+++ kafka-email-service/src/main/java/com/example/emailservice/KafkaEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaEmailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaEmailServiceApplication.class, args);
+    }
+
+}
